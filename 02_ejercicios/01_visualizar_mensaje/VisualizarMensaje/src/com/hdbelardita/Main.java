e35d0cf7c705cebe41f58cb6c4e2eedd143c53bd
Horacio <horaciobelardita@gmail.com>
2019-01-27 16:55:10 -0300

    Primer ejercicio completado

--- 02_ejercicios/01_visualizar_mensaje/VisualizarMensaje/src/com/hdbelardita/Main.java
+++ 02_ejercicios/01_visualizar_mensaje/VisualizarMensaje/src/com/hdbelardita/Main.java
@@ -1,8 +1,15 @@
 package com.hdbelardita;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        System.out.print("Escribe un mensaje: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String mensaje = br.readLine();
+        System.out.println("Has escrito: " + mensaje);
     }
 }
